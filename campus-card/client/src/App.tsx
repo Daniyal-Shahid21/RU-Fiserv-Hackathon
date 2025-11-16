71544f423c9b19b8b3ed58ae154a6c4c1382d42c
AllanRoz <allanrozario22@gmail.com>
2025-11-16 01:36:19 -0500

    Dashboard and Profile

--- campus-card/client/src/App.tsx
+++ campus-card/client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
+import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} /> */}
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
